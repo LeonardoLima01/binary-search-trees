e01ba191768307e9e10ab8f085033f592a2c79c7
socry <leonardosbo02@gmail.com>
2023-03-18 08:58:19 -0300

    Add function to add nodes

--- solution.js
+++ solution.js
@@ -6,34 +6,56 @@ class Node {
   }
 }
 
-class Tree {
-  constructor(root) {
-    this.root = root;
-  }
-  Insert(value) {}
-}
-
 function removeDuplicates(arr) {
   return arr.filter((item, index) => arr.indexOf(item) === index);
 }
 
-function buildTree(arr, start = 0, end = arr.length - 1) {
-  // Sort array
-  arr.sort();
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
+  buildTree(arr, start = 0, end = arr.length - 1) {
+    // Sort array
+    arr.sort();
 
-  // Remove duplicates
-  removeDuplicates(arr);
+    // Remove duplicates
+    removeDuplicates(arr);
 
-  // Base case
-  if (start > end) {
-    return null;
+    // Base case
+    if (start > end) {
+      return null;
+    }
+
+    let mid = parseInt((start + end) / 2);
+    let rootNode = new Node(arr[mid]);
+
+    rootNode.left = this.buildTree(arr, start, mid - 1);
+    rootNode.right = this.buildTree(arr, mid + 1, end);
+
+    return rootNode;
   }
 
-  let mid = parseInt((start + end) / 2);
-  let rootNode = new Node(arr[mid]);
+  Insert(value) {
+    let currentNode = this.root;
+    let newNode = new Node(value);
 
-  rootNode.left = bst(arr, start, mid - 1);
-  rootNode.right = bst(arr, mid + 1, end);
-
-  return rootNode;
+    while (currentNode) {
+      if (value < currentNode.data) {
+        if (currentNode.left == null) {
+          currentNode.left = newNode;
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else if (value > currentNode.data) {
+        if (currentNode.right == null) {
+          currentNode.right = newNode;
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+      console.log(currentNode);
+    }
+  }
 }
