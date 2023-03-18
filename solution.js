5524244eeeb7b950425aeb8a69cf54b231bd3e37
socry <leonardosbo02@gmail.com>
2023-03-18 08:11:44 -0300

    Add buildTree function

--- solution.js
+++ solution.js
@@ -12,3 +12,24 @@ class Tree {
   }
   Insert(value) {}
 }
+
+function buildTree(arr, start = 0, end = arr.length - 1) {
+  // Sort array
+  arr.sort();
+
+  // Remove duplicates
+  removeDuplicates(arr);
+
+  // Base case
+  if (start > end) {
+    return null;
+  }
+
+  let mid = parseInt((start + end) / 2);
+  let rootNode = new Node(arr[mid]);
+
+  rootNode.left = bst(arr, start, mid - 1);
+  rootNode.right = bst(arr, mid + 1, end);
+
+  return rootNode;
+}
